422360d2140a0a68be22713024ca705bf7515d19
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-06 19:49:43 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,6 +2,10 @@ module Findable
   module ClassMethod
   
   def find_by_name(name)
-    all.detect{|a| a.name == name}
+    artists.all.detect{|a| a.name == name}
+  end
+  
+   def find_by_name(name)
+    songs.all.detect{|a| a.name == name}
   end
     
