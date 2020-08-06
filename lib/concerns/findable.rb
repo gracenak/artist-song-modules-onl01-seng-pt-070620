39fc531f5454b91658df907b98c581f0a99fc6c3
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-06 20:27:57 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,11 @@
 module Findable
   module ClassMethod
   
-  def find_by_name(name)
-    all.detect{|a| a.name == name}
+    def find_by_name(name)
+      all.detect{|a| a.name == name}
+    end
   end
+end
   
 
     
